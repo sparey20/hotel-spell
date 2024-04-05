998348064e315be7b66d1b53406e61f1abb05108
Stephen Parey <pareso90@gmail.com>
2024-04-05 10:02:16 -0400

    Adding table title

--- apps/webapp/src/components/table/table.tsx
+++ apps/webapp/src/components/table/table.tsx
@@ -3,6 +3,7 @@ import styles from './table.module.scss';
 export default function Table({ rows }: { rows: any[] }) {
   return (
     <section className={styles.table}>
+      <p>Table</p>
       <ul>
         {rows.map((row) => (
           <li key={row['id']}>{row['id']}</li>
